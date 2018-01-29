74f1c8ba4b5948df2ab9217709b60fa71ff5d6bf
Sam Povilus <povilus@povil.us>
2018-01-28 21:33:58 -0700

    adding code to display temp and humidity, and differentate them

--- humidity.py
+++ humidity.py
@@ -6,6 +6,7 @@ ser = serial.Serial('/dev/ttyACM0',9600)
 ser2 = serial.Serial('/dev/ttyS0',9600)
 brightness_command = "7a"
 cursor_command = "79"
+decimal_command = "77"
 cursor0 = cursor_command + "00"
 ser2.write((brightness_command + "05").decode("hex"))
 loopcount = 0
@@ -13,13 +14,22 @@ while True:
 
         line = ser.readline()
 
-        humi = line.split()[1].split(":")[1]
-        ser2.write(("79000"+
-                    str(humi[0])+ "0" +
-                    str(humi[1])+ "0" +
-                    str(humi[3])+ "0" +
-                    str(humi[4])).decode("hex"))
-        print(humi)
+        
+        humi = line.split()[loopcount%2].split(":")[1]
+        try:
+                ser2.write(("79000"+
+                            str(humi[0])+ "0" +
+                            str(humi[1])+ "0" +
+                            str(humi[3])+ "0" +
+                            str(humi[4])).decode("hex"))
+                if(loopcount%2==1):
+                        ser2.write("7710".decode("hex"))
+                else:
+                        ser2.write("7700".decode("hex"))
+        except TypeError:
+                print("couldn't update display")
+                
+        print(line)
         today = datetime.datetime.now()
         loopcount += 1
         if(loopcount % (20*2) == 0):
