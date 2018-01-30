5f1b5484877457276345d551d599adaa49887108
Sam Povilus <kernel.development@povil.us>
2018-01-29 20:49:00 -0700

    fixed date plotting

--- humidityplot.py
+++ humidityplot.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python
+import matplotlib
 import matplotlib.pyplot as plt
 import re
 import datetime
 import time
 
-humidityarray = []
+humidityarray = [1]
 timearray = []
 with open('temp-humidity.csv') as f:
     lines = f.readlines()
@@ -12,7 +13,7 @@ for line in lines:
     if not re.match(r'^\s*$', line):
         linedate = line.split(",")[0]
         try:
-            linedateobj = time.mktime(datetime.datetime.strptime(linedate,"%Y-%m-%d %H:%M:%S.%f").timetuple())
+            linedateobj = datetime.datetime.fromtimestamp(time.mktime(datetime.datetime.strptime(linedate,"%Y-%m-%d %H:%M:%S.%f").timetuple()))
             print(linedateobj)
             timearray.append(linedateobj)
             locline = line.split(":")[-1].rstrip().translate(None,'%')
@@ -21,6 +22,6 @@ for line in lines:
             pass
 #print(humidityarray)
 
-plt.plot(timearray,humidityarray)
+plt.plot_date(matplotlib.dates.date2num(timearray),(humidityarray[0:-1]))
 #plt.ylabel('some numbers')
 plt.show()
