e7d3df6b6c27b30faecba94d6a6877a89fb8f49a
Sam Povilus <povilus@povil.us>
2018-02-08 20:57:39 -0700

    adding database commits

--- humidity.py
+++ humidity.py
@@ -1,6 +1,23 @@
 #!/usr/bin/env python
 import serial
 import datetime
+import time
+import psycopg2
+
+def add_humidity(humidity, temp):
+                query = """                                                                            
+    INSERT INTO                                                                                
+        temp_humidity                                                                          
+    VALUES                                                                                     
+        (%s, %s, %s)                                                                           
+    """
+                values = ("now",humidity, temp)
+                cur.execute(query, values)
+                conn.commit()
+
+                
+conn = psycopg2.connect('host=pib1 user=pi password=<redacted> dbname=temp_humidity')
+cur = conn.cursor()
 f = open('/home/pi/temp-humidity.csv', 'a')
 ser = serial.Serial('/dev/ttyACM0',9600)
 ser2 = serial.Serial('/dev/ttyS0',9600)
@@ -13,8 +30,9 @@ loopcount = 0
 while True:
 
         line = ser.readline()
-
-        
+        humidity2 = line.split()[1].split(":")[1][0:-1]
+        tempurature2 = line.split()[0].split(":")[1].strip()[0:-2]
+        add_humidity(humidity2,tempurature2)
         humi = line.split()[loopcount%2].split(":")[1]
         try:
                 ser2.write(("79000"+
