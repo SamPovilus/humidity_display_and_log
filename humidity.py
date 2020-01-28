1c12b4b60e566c337979c46157429b1c785b3963
Sam Povilus <kernel.development@povil.us>
2020-01-28 01:44:31 +0000

    remove psycopg, influx is working

--- humidity.py
+++ humidity.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python
 import serial
-import datetime
+#import datetime
+from datetime import datetime
 import time
 import psycopg2
 import password
 import codecs
+from influxdb import InfluxDBClient
 
 def add_humidity(humidity, temp):
                 query = """                                                                            
@@ -18,8 +20,6 @@ def add_humidity(humidity, temp):
                 conn.commit()
 
                 
-conn = psycopg2.connect('host=pib1 user=pi password=' + password.get_password() + ' dbname=humidity_django_db')
-cur = conn.cursor()
 f = open('/home/pi/temp-humidity.csv', 'a')
 ser = serial.Serial('/dev/ttyACM0',9600)
 ser2 = serial.Serial('/dev/ttyS0',9600)
@@ -29,16 +29,21 @@ decimal_command = "77"
 cursor0 = cursor_command + "00"
 ser2.write(codecs.decode((brightness_command + "02"),"hex"))
 loopcount = 0
+
 while True:
 
         line = ser.readline().decode()
         humidity2 = line.split()[1].split(":")[1][0:-1]
         tempurature2 = line.split()[0].split(":")[1].strip()[0:-2]
+        print("humidity2:" + humidity2);
+        print("temp2:" +  tempurature2);
+        
         humi = line.split()[loopcount%2].split(":")[1]
         print(humi[1])
         print(humi[2])
         print(humi[3])
         print(humi[4])
+
         try:
                 ser2.write(("79000"+
                             str(humi[0])+ "0" +
@@ -56,3 +61,5 @@ while True:
         loopcount += 1
         if(loopcount % (60) == 0):
                 add_humidity(humidity2,tempurature2)
+
+
